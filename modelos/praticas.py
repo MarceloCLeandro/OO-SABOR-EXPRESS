630b1f094ea62af6490770156763c5893c252800
MarceloCLeandro <65774706+MarceloCLeandro@users.noreply.github.com>
2024-04-18 20:37:02 -0300

    Property e métodos de classe

--- modelos/praticas.py
+++ modelos/praticas.py
@@ -106,3 +106,126 @@ class Cliente:
 cliente1 = Cliente(nome='Alice', idade=25, email='alice@gmail.com', telefone='123-456-7890')
 cliente2 = Cliente(nome='Bob', idade=30, email='bob@gmail.com', telefone='987-654-3210')
 cliente3 = Cliente(nome='Charlie', idade=22, email='charlie@gmail.com', telefone='555-123-4567')
+
+#---------------------------------------------------------------------------
+class Livro:
+    def __init__(self, titulo ='', autor ='', paginas = 0):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def __str__(self):
+        return f'{self.titulo} por {self.autor} - {self.paginas} páginas'
+
+    @property
+    def titulo_autor(self):
+        return f'{self.titulo} por {self.autor}'
+
+    def aumentar_paginas(self, quantidade):
+        self.paginas += quantidade
+
+# Crie uma nova classe chamada Pessoa com atributos como nome, idade e profissão. 
+#Adicione um método especial __str__ para imprimir uma representação em string da pessoa. 
+#Implemente também um método de instância chamado aniversario que aumenta a idade da pessoa em um ano. 
+#Por fim, adicione uma propriedade chamada saudacao que retorna uma mensagem de saudação personalizada com base na 
+#profissão da pessoa.
+        
+class Pessoa:
+    def __init__(self, nome ='', idade = 0, profissao = ''):
+        self.nome = nome
+        self.idade = idade
+        self.profissao = profissao
+
+    def __str__(self):
+        return f'{self.nome}, {self.idade} anos, {self.profissao}'
+
+    def aniversario(self):
+        self.idade += 1
+        
+    @property
+    def saudacao(self):
+        if self.profissao:
+            return f'Olá, me chamo {self.nome}! Tenho {self.idade} anos e Trabalho com {self.profissao}'
+        else:
+            return f'Olá, me chamo {self.nome}!'
+        
+    
+
+#--------------------------------------------------------------------------------
+
+# Exercícios 3
+
+# 1) Crie uma classe chamada ContaBancaria com um construtor que aceita os parâmetros titular e saldo. Inicie o atributo ativo como False por padrão.
+class ContaBancaria:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+        self._ativo = False
+
+# 2) Na classe ContaBancaria, adicione um método especial __str__ que retorna uma mensagem formatada com o titular e o saldo da conta. Crie duas instâncias da classe e imprima essas instâncias.
+def __str__(self):
+    return f'Conta de {self.titular} - Saldo: R${self.saldo}'
+
+conta1 = ContaBancaria('Pedro', 100)
+conta2 = ContaBancaria('Ana', 300)
+
+print(conta1)
+print(conta2)
+
+# 3) Adicione um método de classe chamado ativar_conta à classe ContaBancaria que define o atributo ativo como True. Crie uma instância da classe, chame o método de classe e imprima o valor de ativo.
+@classmethod
+def ativar_conta(cls, conta):
+    conta._ativo = True
+
+conta3 = ContaBancaria('Joana', 1000)
+print(f'Antes de ativar: Conta ativa? {conta3._ativo}')
+ContaBancaria.ativo_conta(conta3)
+print(f'Depois de ativar: Conta ativa? {conta3._ativo}')
+
+# 4) Refatore a classe ContaBancaria para utilizar a abordagem "pythonica" na criação de atributos. Utilize propriedades, se necessário.
+class ContaBancariaPythonica:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+        self._ativo = False
+
+    @property
+    def titular(self):
+        return self._titular
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def ativo(self):
+        return self._ativo
+
+# 5) Crie uma instância da classe e imprima o valor da propriedade titular.
+conta4 = ContaBancariaPythonica('Fernanda', 1500)
+print(f'Titular da conta 4: {conta4.titular}')
+
+# 6) Crie uma classe chamada ClienteBanco com um construtor que aceita 5 atributos. Instancie 3 objetos desta classe e atribua valores aos seus atributos através do método construtor.
+class ClienteBanco:
+    def __init__(self, nome, idade, endereco, cpf, profissao):
+        self.nome = nome
+        self.idade = idade
+        self.endereco = endereco
+        self.cpf = cpf
+        self.profissao = profissao
+
+cliente1 = ClienteBanco("Ana", 30, "Rua A", "123.456.789-01", "Backend")
+cliente2 = ClienteBanco("Luiza", 25, "Rua B", "987.654.321-01", "Estudante")
+cliente3 = ClienteBanco("Vinny Neves", 40, "Rua C", "111.222.333-44", "Frontend")
+
+# 7) Crie um método de classe para a conta ClienteBanco.
+class ClienteBanco:
+    @classmethod
+    def criar_conta(cls, titular, saldo_inicial):
+        conta = ContaBancariaPythonica(titular, saldo_inicial)
+        return conta
+
+conta_cliente1 = ClienteBanco.criar_conta("Ana", 2000)
+print(f"Conta de {conta_cliente1.titular} criada com saldo inicial de R${conta_cliente1.saldo}")
+
+
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,21 +4,37 @@ class Restaurante:
 
     #atributos da classe
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
+        
+    def _str_(self): return f'{self._nome} {self._categoria}'
 
-    def __str__(self):
-        return f' {self.nome} | {self.categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+    #Métodos da Classe
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {restaurante.ativo}')
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
 
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+    #Atributos Especiais 
+    @property
+    def ativo(self):
+        return '✔️' if self._ativo else '❌'
+
+
+    
+    #Métodos para os Objs
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+
+restaurante_pizza = Restaurante('pizza Express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
